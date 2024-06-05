119562f958f2a13b3e7bd7daa06aac13b8be792d
beenamgrg <beenamgrg089@gmail.com>
2024-06-05 11:34:15 +0545

    code review

--- app/Http/Controllers/JobApplicationController.php
+++ app/Http/Controllers/JobApplicationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\JobApplication;
+use App\Models\JobListing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
@@ -28,23 +29,34 @@ class JobApplicationController extends Controller
             {
                 return response(['errors' => $validator->errors()->all()], 422);
             }
-            $check = JobApplication::where('id', $request->job_id)->where('user_id', Auth::user()->id)->firstORFail() ?? NULL;
-            if ($check != NULL)
+
+            //Check if the job exsists
+            $jobCheck = JobListing::where('id', $request->job_id)->first() ?? NULL;
+            if ($jobCheck == NULL)
             {
-                $response = APIHelpers::createAPIResponse(false, 400, 'You have already applied for this job before!', Auth::user()->name);
+                $response = APIHelpers::createAPIResponse(true, 400, 'The job doesnot exsist!!', Auth::user()->name);
                 return response()->json($response, 400);
             }
 
-            // Create a unique name for the image
-            $path = public_path() . '/job-applications/resume/';
-            if (!file_exists($path))
+            //Check if the user has applied the job before
+            $check = JobApplication::where('job_id', $request->job_id)->where('user_id', Auth::user()->id)->first() ?? NULL;
+            if ($check != NULL)
             {
-                mkdir($path, 0777, true);
+                $response = APIHelpers::createAPIResponse(true, 400, 'You have already applied for this job before!', Auth::user()->name);
+                return response()->json($response, 400);
+            }
+
+            // Create a unique name for the file and move the file to public path
+            $path = '/job-applications/resume/';
+            if (!file_exists(public_path() . $path))
+            {
+                mkdir(public_path() . $path, 0777, true);
             }
             $fileName =  $path . time() . '.' . Str::random(4) . $request->resume->extension();
             $file = $request->file('resume');
-            // dd($path);
-            $file->move($path, $fileName);
+            $file->move(public_path() . $path, $fileName);
+
+
             $job_application = new JobApplication();
             $job_application->resume = $fileName;
             $job_application->cover_letter = $request->cover_letter;
